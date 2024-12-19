155ce6d52f81ae2ceddd131b3f0013c6ae4a46ac
Jonathan Urias <jonathan@ycharts.com>
2024-12-19 12:08:58 -0700

    updated

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,46 +3,38 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/jonathanYcharts/profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/LondonAppDev/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Set Ubuntu Language
-locale-gen en_GB.UTF-8
-
-# Install Python, SQLite, and required packages
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git build-essential libssl-dev
+apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
-# Create project directory and clone the repo
+# Create project directory
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
-# Set up Python virtual environment
+# Create virtual environment
+mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
-# Upgrade pip, setuptools, and wheel
-echo "Upgrading pip, setuptools, and wheel..."
-$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip setuptools wheel
+# Install python packages
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
-# Install project dependencies, including uWSGI
-echo "Installing project dependencies..."
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Run migrations and collectstatic
+cd $PROJECT_BASE_PATH
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
-# Run database migrations
-echo "Running migrations..."
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
-
-# Set up Supervisor to manage uWSGI process
-echo "Configuring Supervisor..."
+# Configure supervisor
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Set up Nginx to serve the application
-echo "Configuring Nginx..."
+# Configure nginx
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
