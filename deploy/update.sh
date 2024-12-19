94778c22b9ff8dba52b3dffc768326b1b1cd5b67
Jonathan Urias <jonathan@ycharts.com>
2024-12-19 12:36:37 -0700

    update file

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,9 +4,27 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+# Activate the virtual environment
+source $PROJECT_BASE_PATH/env/bin/activate
+
+# Pull the latest changes from the repository
+echo "Pulling latest changes..."
+git -C $PROJECT_BASE_PATH pull
+
+# Upgrade Django REST Framework and dependencies
+echo "Upgrading dependencies..."
+pip install --upgrade djangorestframework six
+
+# Run database migrations
+echo "Running migrations..."
+python $PROJECT_BASE_PATH/manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python $PROJECT_BASE_PATH/manage.py collectstatic --noinput
+
+# Restart Supervisor
+echo "Restarting Supervisor..."
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
